56c388da7b301b875a86fcc926b13f662bc6b493
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2022-03-27 23:00:48 -0600

    update-acomtus

--- includes/backup.bat
+++ includes/backup.bat
@@ -1,5 +1,5 @@
 cls
 SET PGPASSWORD=Orellana
-pg_dump -U postgres -v -F c acomtus > c:\wamp64\www\sistema_acomtus.dump
+pg_dump -U postgres -v -F c acomtus > c:\xampp\sistema_acomtus.dump
 cls
-xcopy c:\wamp64\www\sistema_acomtus.dump "G:\Mi unidad\Respaldo" /y
+xcopy c:\xampp\sistema_acomtus.dump "G:\Mi unidad\Respaldo" /y
