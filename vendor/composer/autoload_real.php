0cde7d3ac73b76848a833115e52d386e97f3204f
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2024-04-14 22:42:21 -0600

    udpate-varios

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit320a1571161954caa7af9b73d6429e43
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit320a1571161954caa7af9b73d6429e43::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire320a1571161954caa7af9b73d6429e43($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit320a1571161954caa7af9b73d6429e43::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire320a1571161954caa7af9b73d6429e43($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
